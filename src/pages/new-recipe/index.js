cc35a723a1444cc93af91cb0b28b46b911231bd1
Iliana Karakasheva <karakashevailiana@gmail.com>
2023-02-27 15:26:16 +0200

    added post recipe form

--- src/pages/new-recipe/index.js
+++ src/pages/new-recipe/index.js
@@ -6,11 +6,48 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function NewRecipe() {
-  return (
-<div className='newRecipe'>
-    <h4>post a new recipe</h4>
+    return (
+        <div className='container'>
+            <div className='newRecipe row align-items-center justify-content-center mt-5'>
+                {/* <h4>post a new recipe</h4> */}
 
+                <div className='image col-6 d-flex'>
+                    Pick a photo of your dish:
+                    <input type="file" />
+                </div>
+                <form className='col-6'>
 
-</div>
-  )
-    }
+                    <div className='title'>
+                        <strong>
+                            Label your dish:
+                        </strong>
+                        <input type="text" className='form-control' placeholder='Dish Title' />
+                    </div>
+                    <div className='content'>
+                        <div className='time'>
+                            <strong>
+                                How long does it take to cook?
+                            </strong>
+                            <input type="text" className='form-control' placeholder='Time Needed' />
+                        </div>
+                        <div className='ingredients'>
+                            <strong>
+                                Ingredients needed:
+                            </strong>
+                            <input type="text" className='form-control' placeholder='Ingredients needed' />
+                        </div>
+                        <div className='instructions'>
+                            <strong>
+                                Instructions:
+                            </strong>
+                            <input type="text" className='form-control' placeholder='Instructions' />
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-sm btn-secondary me-2 mt-4 mb-3">Cancel</button>
+                    <button type="submit" class="btn btn-sm btn-secondary me-2 mt-4 mb-3">Submit Recipe</button>
+
+                </form>
+            </div>
+        </div>
+    )
+}
