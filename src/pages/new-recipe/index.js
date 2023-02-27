c7a120a40a66fb21b3903074082824c647bf2213
Iliana Karakasheva <karakashevailiana@gmail.com>
2023-02-27 11:07:50 +0200

    added recipe post page route+ css changes

---
+++ src/pages/new-recipe/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function NewRecipe() {
+  return (
+<div className='newRecipe'>
+    <h4>post a new recipe</h4>
+
+
+</div>
+  )
+    }
